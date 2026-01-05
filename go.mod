2515c1f95fe02e27ad7c7c91475b367d8634e3d8
Benjamin Schmidt <ben741@users.noreply.github.com>
2026-01-05 14:49:46 -0500

    Use gonum v0.16.0 directly while v0.8.2 referenced indirectly

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.25.4
 
 require github.com/james-bowman/sparse v0.0.0-20210729090128-1e6c7dd483e9
 
-require gonum.org/v1/gonum v0.8.2 // indirect
+require gonum.org/v1/gonum v0.16.0
--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 
 	"github.com/james-bowman/sparse"
+	"gonum.org/v1/gonum"
 )
 
 func main() {
+	v, c := gonum.Version()
+	fmt.Println(v, c)
+
 	vec := sparse.NewVector(0, []int{}, []float64{})
 	fmt.Println(vec.Len())
 }
